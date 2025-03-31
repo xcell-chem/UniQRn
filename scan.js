a47a5d68608fb7e7b4db6a71c466b296ac1fddd8
xcell-chem <xcell.chem91@gmail.com>
2025-03-31 19:24:00 +0100

    Update scan.js

--- scan.js
+++ scan.js
@@ -1,3 +1,15 @@
+
+const currentPath = window.location.pathname + window.location.search;
+const redirectUrl = window.location.origin + '/auth.html?next=' + encodeURIComponent(currentPath);
+
+supabase.auth.signInWithOAuth({
+  provider: 'google',
+  options: {
+    redirectTo: redirectUrl
+  }
+});
+
+
 function login() {
   const currentPath = window.location.pathname + window.location.search;
   const redirectUrl = window.location.origin + '/auth.html?next=' + encodeURIComponent(currentPath);
