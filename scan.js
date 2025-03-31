a2d46231f79a58cea36224ca5ddaa5812a7bd2cc
xcell-chem <xcell.chem91@gmail.com>
2025-03-31 19:18:27 +0100

    hjj

--- scan.js
+++ scan.js
@@ -1,10 +1,11 @@
+function login() {
+  const currentPath = window.location.pathname + window.location.search;
+  const redirectUrl = window.location.origin + '/auth.html?next=' + encodeURIComponent(currentPath);
 
-const currentPath = window.location.pathname + window.location.search;
-const redirectUrl = window.location.origin + '/auth.html?next=' + encodeURIComponent(currentPath);
-
-supabase.auth.signInWithOAuth({
-  provider: 'google',
-  options: {
-    redirectTo: redirectUrl
-  }
-});
+  supabase.auth.signInWithOAuth({
+    provider: 'google',
+    options: {
+      redirectTo: redirectUrl
+    }
+  });
+}
